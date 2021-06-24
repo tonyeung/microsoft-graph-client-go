81e5af1d56307aa0a5673efbe07b7d452104ff44
Ton Yeung <RDuong@slb.com>
2021-06-24 16:06:33 -0500

    added remaining queries and tests

--- graph/query/query.go
+++ graph/query/query.go
@@ -8,18 +8,90 @@ import (
 
 type QueryOption func(url url.URL) url.URL
 
-func Top(count int) QueryOption {
+func Version(version string) QueryOption {
+	return func(u url.URL) url.URL {
+		u.Path = strings.Replace(u.Path, "v1.0", version, 1)
+		return u
+	}
+}
+
+func Count(b bool) QueryOption {
 	return func(u url.URL) url.URL {
 		queryString, _ := url.ParseQuery(u.RawQuery)
-		queryString.Add("$top", strconv.Itoa(count))
+		queryString.Add("$count", strconv.FormatBool(b))
 		u.RawQuery = queryString.Encode()
 		return u
 	}
 }
 
-func Version(version string) QueryOption {
+func Expand(resource string) QueryOption {
 	return func(u url.URL) url.URL {
-		u.Path = strings.Replace(u.Path, "v1.0", version, 1)
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$expand", resource)
+		u.RawQuery = queryString.Encode()
+		return u
+	}
+}
+
+func Filter(filter string) QueryOption {
+	return func(u url.URL) url.URL {
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$filter", filter)
+		u.RawQuery = queryString.Encode()
+		return u
+	}
+}
+
+func Format(format string) QueryOption {
+	return func(u url.URL) url.URL {
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$format", format)
+		u.RawQuery = queryString.Encode()
+		return u
+	}
+}
+
+func OrderBy(order string) QueryOption {
+	return func(u url.URL) url.URL {
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$orderby", order)
+		u.RawQuery = queryString.Encode()
+		return u
+	}
+}
+
+func Search(term string) QueryOption {
+	return func(u url.URL) url.URL {
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$search", term)
+		u.RawQuery = queryString.Encode()
+		return u
+	}
+}
+
+func Select(fields string) QueryOption {
+	return func(u url.URL) url.URL {
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$select", fields)
+		u.RawQuery = queryString.Encode()
+		return u
+	}
+}
+
+func Skip(items int) QueryOption {
+	return func(u url.URL) url.URL {
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$skip", strconv.Itoa(items))
+		u.RawQuery = queryString.Encode()
+		return u
+	}
+}
+
+func Top(count int) QueryOption {
+	return func(u url.URL) url.URL {
+		queryString, _ := url.ParseQuery(u.RawQuery)
+		queryString.Add("$top", strconv.Itoa(count))
+		u.RawQuery = queryString.Encode()
 		return u
 	}
 }
--- graph/query/query_test.go
+++ graph/query/query_test.go
@@ -7,31 +7,143 @@ import (
 	"github.com/tonyeung/microsoft-graph-client-go/graph/query"
 )
 
-func TestTop(t *testing.T) {
-	u := url.URL{
-		Scheme: "https",
-		Host:   "slb.com",
-	}
-	q := query.Top(1)
-	u = q(u)
-
-	if u.String() != "https://slb.com?%24top=1" {
-		t.Error("TestTop failed, expected https://slb.com?%24top=1, got:" + u.String())
-		return
-	}
-}
-
 func TestVersion(t *testing.T) {
 	u := url.URL{
 		Scheme: "https",
-		Host:   "slb.com",
+		Host:   "google.com",
 		Path:   "v1.0",
 	}
 	q := query.Version("foo")
 	u = q(u)
 
-	if u.String() != "https://slb.com/foo" {
-		t.Error("TestTop failed, expected https://slb.com/foo, got:" + u.String())
+	if u.String() != "https://google.com/foo" {
+		t.Error("TestTop failed, expected https://google.com/foo, got:" + u.String())
+		return
+	}
+}
+
+func TestCount(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Count(true)
+	u = q(u)
+
+	if u.String() != "https://google.com?%24count=true" {
+		t.Error("TestTop failed, expected https://google.com?%24count=true, got:" + u.String())
+		return
+	}
+}
+
+func TestExpand(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Expand("members")
+	u = q(u)
+
+	if u.String() != "https://google.com?%24expand=members" {
+		t.Error("TestTop failed, expected https://google.com?%24expand=members, got:" + u.String())
+		return
+	}
+}
+
+func TestFilter(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Filter("filter")
+	u = q(u)
+
+	if u.String() != "https://google.com?%24filter=filter" {
+		t.Error("TestTop failed, expected https://google.com?%24filter=filter, got:" + u.String())
+		return
+	}
+}
+
+func TestFormat(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Format("json")
+	u = q(u)
+
+	if u.String() != "https://google.com?%24format=json" {
+		t.Error("TestTop failed, expected https://google.com?%24format=json, got:" + u.String())
+		return
+	}
+}
+
+func TestOrderBy(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.OrderBy("displayName desc")
+	u = q(u)
+
+	if u.String() != "https://google.com?%24orderby=displayName+desc" {
+		t.Error("TestTop failed, expected https://google.com?%24orderby=displayName+desc, got:" + u.String())
+		return
+	}
+}
+
+func TestSearch(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Search("pizza")
+	u = q(u)
+
+	if u.String() != "https://google.com?%24search=pizza" {
+		t.Error("TestTop failed, expected https://google.com?%24search=pizza, got:" + u.String())
+		return
+	}
+}
+
+func TestSelect(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Select("givenName,surname")
+	u = q(u)
+
+	if u.String() != "https://google.com?%24select=givenName%2Csurname" {
+		t.Error("TestTop failed, expected https://google.com?%24select=givenName%2Csurname, got:" + u.String())
+		return
+	}
+}
+
+func TestSkip(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Skip(11)
+	u = q(u)
+
+	if u.String() != "https://google.com?%24skip=11" {
+		t.Error("TestTop failed, expected https://google.com?%24skip=11, got:" + u.String())
+		return
+	}
+}
+
+func TestTop(t *testing.T) {
+	u := url.URL{
+		Scheme: "https",
+		Host:   "google.com",
+	}
+	q := query.Top(1)
+	u = q(u)
+
+	if u.String() != "https://google.com?%24top=1" {
+		t.Error("TestTop failed, expected https://google.com?%24top=1, got:" + u.String())
 		return
 	}
 }
